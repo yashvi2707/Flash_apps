2b62944ae32713f3dd1aa9b58896146118c94ba8
yashvi2707 <128456166+yashvi2707@users.noreply.github.com>
2025-01-08 07:16:44 +0000

    Added html files
    In this pyhton file is connected to html files

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template,jsonify
+
+app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+  'title':'Data Analyst',
+  'location':'Bengaluru, India',
+    'salary':'Rs.10,00,000'
+  },
+
+  {
+    'id': 2,
+  'title':'Data Scientist',
+  'location':'Delhi, India',
+    'salary':'Rs.15,50,000'
+  },
+  {
+    'id': 3,
+  'title':'Frontend Engineer',
+  'location':'Remote',
+    'salary':'Rs.12,00,000'
+  },
+
+{
+  'id': 4,
+'title':'Backend Engineer',
+'location':'remoted',
+  'salary':'Rs.16,00,000'
+}
+
+]
+
+@app.route('/')
+def hello_world():
+  return render_template('home.html',jobs=JOBS, company_name='CubeX')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0',debug=True)
